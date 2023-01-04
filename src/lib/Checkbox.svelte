9954ca82125442c6bcb117a7ad6c169a044d0ced
Vincent <vincentvibe3@gmail.com>
2023-01-04 14:16:47 -0500

    Fix checkbox animations

--- src/lib/Checkbox.svelte
+++ src/lib/Checkbox.svelte
@@ -4,11 +4,14 @@
 	export let name:string="";
 	export let checked = false;
 	let checkClass = "";
+	let checkMark:HTMLDivElement
 
 	$: if (checked){
 		checkClass = "checked"
-	} else {
+		checkMark.classList.add("animate")
+	} else if (checkMark!=null) {
 		checkClass = ""
+		checkMark.classList.remove("animate")
 	}
 
 	const onClick = () => {
@@ -19,7 +22,7 @@
 <input bind:checked={checked} on:click name={name}  type="checkbox" bind:this={htmlElement}>
 <div style={$$props.style} class="wrapper">
 	<button tabindex="0" on:click={onClick} class="checkboxWrapper {checkClass}">
-		<div class="customCheckmark">
+		<div bind:this={checkMark} class="customCheckmark">
 			<!-- check icon from phosphor icons -->
 			<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#fafafa" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="216 72 104 184 48 128" fill="none" stroke="#fafafa" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></polyline></svg>
 		</div>
@@ -44,9 +47,7 @@
 		border-radius: 1.5rem;
 		background-color: #99003b;
 		border: none;
-		animation-name: fadeInCircle;
-		animation-timing-function: ease-in-out;
-		animation-duration: 0.2s;
+		transition: all linear 0.1s;
 	}
 
 	.checkboxWrapper {
@@ -57,9 +58,10 @@
 		border: #bf5383 solid 0.2rem;
 		background-color: #00000000;
 		cursor: pointer;
-		animation-name: fadeInCircle;
-		animation-timing-function: ease-in-out;
-		animation-duration: 0.2s;
+	}
+
+	.customCheckmark.animate{
+		animation: fillAnimation ease-in-out 0.2s;
 	}
 
 	.customCheckmark {
@@ -69,24 +71,12 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		animation-name: fillAnimation;
-		animation-timing-function: ease-in-out;
-		animation-duration: 0.2s;
-	}
-
-	@keyframes fadeInCircle {
-		0% {
-			opacity: 0.5;
-		}
-		100% {
-			opacity: 1;
-		}
 	}
 
 	@keyframes fillAnimation {
 		0% {
-			opacity: 0;
-			transform: rotate(30deg);
+			opacity: 0.5;
+			transform: rotate(60deg);
 		}
 		100% {
 			opacity: 1;
@@ -95,8 +85,8 @@
 	}
 
 	svg {
-		width: 60%;
-		height: 60%;
+		width: 100%;
+		height: 100%;
 	}
 
 	.wrapper{
