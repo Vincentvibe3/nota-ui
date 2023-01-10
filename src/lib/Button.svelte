6cdb94197cd97994d1576974157c3494e7fbd214
Vincent <vincentvibe3@gmail.com>
2023-01-10 18:22:56 -0500

    add more variants for buttons

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,31 +1,17 @@
 <script lang="ts">
 	export let htmlElement: HTMLButtonElement | null = null;
-	export let type: String = 'primary';
+	export let type: string = 'primary';
+	export let disabled:boolean=false;
 </script>
 
-{#if type == 'secondary'}
-	<button class="secondary" style={$$props.style} bind:this={htmlElement} on:click>
-		{#if $$slots.icon}
-			<div class="iconWrapper">
-				<slot name="icon"/>
-			</div>
-		{/if}
-		<slot/>
-	</button>
-{:else if type == 'primary'}
-	<button class="primary" style={$$props.style} bind:this={htmlElement} on:click>
-		{#if $$slots.icon}
-			<div class="iconWrapper">
-				<slot name="icon"/>
-			</div>
-		{/if}
-		<slot/>
-	</button>
-{:else if type=="icon"}
-	<button class="icon" style={$$props.style} bind:this={htmlElement} on:click>
-		<slot name="icon"/>
-	</button>
-{/if}
+<button disabled={disabled} class={type} style={$$props.style} bind:this={htmlElement} on:click>
+	{#if $$slots.icon}
+		<div class="iconWrapper">
+			<slot name="icon"/>
+		</div>
+	{/if}
+	<slot/>
+</button>
 
 <style>
 	button {
@@ -41,6 +27,20 @@
 		transition: all ease-in-out 0.2s;
 		font: var(--btnFont, 600 1rem sans-serif);
 	}
+
+	button.primary:disabled, button.secondary:disabled, 
+	button.tertiary:disabled, button.danger:disabled, 
+	button.primary:disabled:hover, button.secondary:disabled:hover, 
+	button.tertiary:disabled:hover, button.danger:disabled:hover,
+	button.primary:disabled:focus-visible, button.secondary:disabled:focus-visible, 
+	button.tertiary:disabled:focus-visible, button.danger:disabled:focus-visible,
+	button.primary:disabled:active, button.secondary:disabled:active, 
+	button.tertiary:disabled:active, button.danger:disabled:active{
+		background-color: var(--btnDisabledBg, #f0f0f0);
+		border: var(--btnDisabledBorder, #c0c0c0) solid 0.15rem;
+		color: var(--btnDisabledText, #909090);
+	}
+
 
 	button.icon {
 		outline: none;
@@ -70,31 +70,49 @@
 		width: 1rem;
 	}
 
-	button.secondary {
+	button.tertiary {
 		border: none;
-		color: var(--btnSecondaryText, #b3386b);
-		background-color: #00000000;
+		color: var(--btnTertiaryText, #b3386b);
+		background-color: var(--btnTertiaryBg, transparent);
 	}
 
-	button.primary {
-		border: none;
+	button.tertiary:hover {
+		background-color: var(--btnTertiaryBgFocus, #f2c2e3);
+	}
+
+	button.tertiary:focus-visible {
+		background-color: var(--btnTertiaryBgFocus, #f2c2e3);
 		outline: none;
-		color: var(--btnPrimaryText, #ffffff);
-		background-color: var(--btnPrimaryBg, #b3386b);
-		border: var(--btnPrimaryBorder, #bf5383) solid 0.15rem;
+	}
+
+	button.tertiary:active {
+		background-color: var(--btnTertiaryBgFocus, #f2c2e3);
+	}
+
+	button.secondary {
+		border: var(--btnSecondaryBorder, #a31c54) solid 0.1rem;
+		color: var(--btnSecondaryText, #a31c54);
+		background-color: var(--btnSecondaryBg, transparent);
 	}
 
 	button.secondary:hover {
-		background-color: var(--btnSecondaryBgFocus, #b3386b26);
+		background-color: var(--btnSecondaryBgFocus, #f2c2e3);
 	}
 
 	button.secondary:focus-visible {
-		background-color: var(--btnSecondaryBgFocus, #b3386b26);
+		background-color: var(--btnSecondaryBgFocus, #f2c2e3);
 		outline: none;
 	}
 
 	button.secondary:active {
-		background-color: var(--btnSecondaryBgFocus, #b3386b26);
+		background-color: var(--btnSecondaryBgFocus, #f2c2e3);
+	}
+
+	button.primary {
+		outline: none;
+		color: var(--btnPrimaryText, #ffffff);
+		background-color: var(--btnPrimaryBg, #b3386b);
+		border: var(--btnPrimaryBorder, #bf5383) solid 0.15rem;
 	}
 
 	button.primary:hover {
@@ -107,7 +125,25 @@
 		border: var(--btnPrimaryBorderFocus, #a31c54) solid 0.15rem;
 	}
 
-	button:active {
+	button.danger {
+		border: none;
+		outline: none;
+		color: var(--btnDangerText, #ffffff);
+		background-color: var(--btnDangerBg, #db5c5c);
+		border: var(--btnDangerBorder, #db8484) solid 0.15rem;
+	}
+
+	button.danger:hover {
+		background-color: var(--btnDangeryBgFocus, #db3434);
+		border: var(--btnDangerBorderFocus, #db5c5c) solid 0.15rem;
+	}
+
+	button.danger:focus-visible {
+		background-color: var(--btnDangerBgFocus, #db3434);
+		border: var(--btnDangerBorderFocus, #db5c5c) solid 0.15rem;
+	}
+
+	button:active:not(:disabled) {
 		box-shadow: #00000033 0.2rem 0.2rem 1rem;
 		filter: brightness(85%);
 	}
