8b766a2431c0e4c0d0aaf4a6f8de8a2e882934a7
Vincent <vincentvibe3@gmail.com>
2023-01-10 18:21:16 -0500

    add backdrop

---
+++ src/lib/Backdrop.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let show:boolean=false;
+</script>
+<div on:click class:show={show} style={$$props.style}></div>
+<style>
+	div {
+		position: fixed;
+		top:0px;
+		left:0px;
+		width: 100vw;
+		height: 100vh;
+		background-color: var(--n900, #000000);
+		opacity: 0;
+		transition: all ease-in-out 0.2s;
+		display: none;
+	}
+
+	div.show{
+		opacity: 0.5;
+		display: block;
+	}
+</style>
