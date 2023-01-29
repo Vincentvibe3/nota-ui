387cc59f7d15b9c6ad499d23a3ee57dc1cea9c25
Vincent <vincentvibe3@gmail.com>
2023-01-29 17:55:02 -0500

    add title

--- src/routes/(app)/components/+page.svelte
+++ src/routes/(app)/components/+page.svelte
@@ -3,6 +3,7 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
+<svelte:head><title>Components</title></svelte:head>
 <Header>Components</Header>
 <main>
 	{#each data.names as name}
