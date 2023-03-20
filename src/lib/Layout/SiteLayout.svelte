a0cfbaa92aed0f2cf1594e0dd4f9e851f36f4b8f
Vincent <vincentvibe3@gmail.com>
2023-03-19 23:02:43 -0400

    start laying out sitelayout component

---
+++ src/lib/Layout/SiteLayout.svelte
@@ -0,0 +1,13 @@
+<div class="siteContent">
+	<slot name="navbar"></slot>
+	<slot name="leftSidebar"></slot>
+	<slot></slot>
+	<slot name="footer"></slot>
+</div>
+<style lang="scss">
+	.siteContent {
+		display: grid;
+		grid-template-rows: 3.5rem 1fr 1fr;
+		grid-template-columns: 20rem 1fr;
+	}
+</style>
--- src/lib/Navigation/Sidebar.svelte
+++ src/lib/Navigation/Sidebar.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-	import { setContext } from "svelte";
+	import { onMount, setContext } from "svelte";
 	import { writable } from "svelte/store";
 	import {Backdrop} from "../Backdrop";
-	import { fade } from "svelte/transition";
+	import { fade, slide } from "svelte/transition";
+	import { ContentContainer } from "$lib/Layout";
 
-	export let show=true;
+	export let show=false;
 
 	let sidebar:HTMLButtonElement;
 
 	const showStore = writable(show)
+
+	let desktop = false
+
+	onMount(()=>{
+		let query = window.matchMedia("(min-width: 1000px)")
+		query.addEventListener("change", (query)=>{
+			desktop = query.matches
+			show=false
+		})
+		desktop = query.matches
+	})
 
 	$:showStore.update(_=>show)
 	$:show = $showStore
@@ -26,22 +38,25 @@
 	}
 
 </script>
-
-<div class="wrapper" class:show={show}>
-	<Backdrop on:click={close} bind:show={$showStore}></Backdrop>
-	<div class:sidebar={true} class:show={$showStore}>
-		{#if show}
-			<button out:fade="{{duration:300}}" bind:this={sidebar} on:click={close} class="close" disabled={!show}>
-				<div style="height: 40%; width:auto; margin:0rem; margin-right:1rem;">
-					<!-- Icon from Phosphor Icons (https://phosphoricons.com/) licensed under an MIT license -->
-					<svg style="height:100%; width:auto;" xmlns="http://www.w3.org/2000/svg" width="192" height="192"  viewBox="0 0 256 256"><rect width="256" height="256" fill="none" stroke="none"></rect><line x1="200" y1="56" x2="56" y2="200"  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="200" y1="200" x2="56" y2="56"  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
-				</div>
-				Close
-			</button>
-			<div out:fade="{{duration:300}}" class="content">
-				<slot></slot>
+{#if show &&!desktop}
+<div class="backdropWrapper">
+	<Backdrop show={show&&!desktop} on:click={close}/>
+</div>
+{/if}
+<div class:sidebar={true} class:show={$showStore||desktop}>
+	{#if !desktop}
+		<button out:fade="{{duration:300}}" bind:this={sidebar} on:click={close} class="close" disabled={!show}>
+			<div style="height: 40%; width:auto; margin:0rem; margin-right:1rem;">
+				<!-- Icon from Phosphor Icons (https://phosphoricons.com/) licensed under an MIT license -->
+				<svg style="height:100%; width:auto;" xmlns="http://www.w3.org/2000/svg" width="192" height="192"  viewBox="0 0 256 256"><rect width="256" height="256" fill="none" stroke="none"></rect><line x1="200" y1="56" x2="56" y2="200"  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="200" y1="200" x2="56" y2="56"  stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
 			</div>
-		{/if}
+			Close
+		</button>
+	{/if}
+	<div class="content" class:desktop>
+		<ContentContainer --contentContainerGap="0.25rem" --contentContainerPaddingx="1rem"  direction="column">
+			<slot></slot>
+		</ContentContainer>
 	</div>
 </div>
 
@@ -94,16 +109,38 @@
 		}
 	}
 
-	.wrapper {
+	.sidebar{
 		position: fixed;
-		z-index: 4;
+		z-index: 3;
+		left:-20rem;
 	}
 
-	.sidebar {
+	.backdropWrapper{
 		position: fixed;
-		top: 0px;
-		left:0px;
+		z-index: 2;
+	}
+	
+	@media only screen and (min-width: 1000px) {
+		.sidebar {
+			z-index: unset;
+			min-width:20rem;
+			height: 100%;
+			flex: 0 0 20rem;
+			left:0rem;
 
+			.content {
+				padding-top: 4.5rem;
+			}
+		}
+
+		.close {
+			display: none;
+		}
+
+	} 
+
+	.sidebar {
+		top:0rem;
 		width:20rem;
 		height: 100vh;
 
@@ -111,28 +148,26 @@
 		align-items: center;
 		justify-content: start;
 		flex-direction: column;
-
-		z-index: inherit;
 		
-		// overflow-y: scroll;
+		overflow-y: scroll;
 
 		background-color: var(--sidebarBg, #f0f0f0);
-
-		transform: translateX(-100%);
 		
 		transition: all ease 0.2s;
+
+		gap: 1rem;
+
+		&.show{
+			left:0rem;
+		}
 
 		.content {
 			width:100%;
 			background-color: var(--sidebarBg, #f0f0f0);
 			overflow-y: scroll;
+
 		}
 
-		&.show{
-			transform: translateX(0%);
-		}
 	}
-
-
 
 </style>
