1b05dc203b916ad8634a425b2a3c0ace8f062d20
Vincent <vincentvibe3@gmail.com>
2023-03-24 00:13:22 -0400

    format Modal Controller

--- src/lib/Modal/ModalController.svelte
+++ src/lib/Modal/ModalController.svelte
@@ -1,29 +1,35 @@
 <script lang="ts" context="module">
-
-	export const modalQueue:Writable<{component:any, title:string, strict?:boolean}[]> = writable([])
+	export const modalQueue: Writable<{ component: any; title: string; strict?: boolean }[]> =
+		writable([]);
 
 	export const dismissModal = () => {
-		console.log("dismissing")
-		modalQueue.update(queue => {queue.splice(0, 1); return queue})
-	}
+		modalQueue.update((queue) => {
+			queue.splice(0, 1);
+			return queue;
+		});
+	};
 
-	export const queueModal = (component:{component:any, strict?:boolean, title:string}) => {
-		if (component.strict===undefined){
-			component.strict = false
+	export const queueModal = (component: { component: any; strict?: boolean; title: string }) => {
+		if (component.strict === undefined) {
+			component.strict = false;
 		}
-		modalQueue.update(queue => {queue.push(component); return queue})
-	}
+		modalQueue.update((queue) => {
+			queue.push(component);
+			return queue;
+		});
+	};
+</script>
 
-</script>
 <script lang="ts">
-	import { writable, type Writable } from "svelte/store";
-	import Modal from "./Modal.svelte";
+	import { writable, type Writable } from 'svelte/store';
+	import Modal from './Modal.svelte';
 </script>
+
 {#if $modalQueue.length != 0}
 	{#key $modalQueue[0].component}
 		<Modal open strict={$modalQueue[0].strict}>
 			<svelte:fragment slot="title">{$modalQueue[0].title}</svelte:fragment>
-			<svelte:component this={$modalQueue[0].component}></svelte:component>
+			<svelte:component this={$modalQueue[0].component} />
 		</Modal>
 	{/key}
 {/if}
